348a6001d5dd9f4db5f058d485108b991feee33c
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2016-06-19 13:12:04 -0400

    Add tutorial page

--- www/js/app.js
+++ www/js/app.js
@@ -30,6 +30,11 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
   $stateProvider
+  
+  .state('tutorial', {
+    url: '/tutorial',
+    templateUrl: 'templates/tutorial.html'
+  })
 
   // setup an abstract state for the tabs directive
     .state('tab', {
@@ -80,6 +85,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tutorial');
 
 });
